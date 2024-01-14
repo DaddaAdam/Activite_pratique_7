15049d2b35e970431c56243fdfc55c0952ec733c
DaddaAdam <daddaadam@gmail.com>
2024-01-14 18:19:04 +0100

    added role based access w/ keycloak

--- customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/sec/SecurityConfig.java
+++ customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/sec/SecurityConfig.java
@@ -3,14 +3,23 @@ package ma.emsi.customerfrontthymeleafapp.sec;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private ClientRegistrationRepository clientRegistrationRepository;
 
@@ -38,5 +47,28 @@ public class SecurityConfig {
                 new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
         oidcClientInitiatedLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8083/");
         return oidcClientInitiatedLogoutSuccessHandler;
+    }
+
+    @Bean
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            final Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+            authorities.forEach((authority) -> {
+                if (authority instanceof OidcUserAuthority oidcAuth) {
+                    mappedAuthorities.addAll(mapAuthorities(oidcAuth.getIdToken().getClaims()));
+                    System.out.println(oidcAuth.getAttributes());
+                } else if (authority instanceof OAuth2UserAuthority oauth2Auth) {
+                    mappedAuthorities.addAll(mapAuthorities(oauth2Auth.getAttributes()));
+                }
+            });
+            return mappedAuthorities;
+        };
+    }
+    private List<SimpleGrantedAuthority> mapAuthorities(final Map<String, Object> attributes) {
+        final Map<String, Object> realmAccess = ((Map<String, Object>)attributes.getOrDefault("realm_access", Collections.emptyMap()));
+        final Collection<String> roles = ((Collection<String>)realmAccess.getOrDefault("roles", Collections.emptyList()));
+        return roles.stream()
+                .map((role) -> new SimpleGrantedAuthority(role))
+                .toList();
     }
 }
--- customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
+++ customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/web/CustomerController.java
@@ -3,6 +3,7 @@ package ma.emsi.customerfrontthymeleafapp.web;
 import lombok.AllArgsConstructor;
 import ma.emsi.customerfrontthymeleafapp.entities.Customer;
 import ma.emsi.customerfrontthymeleafapp.repository.CustomerRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,6 +18,7 @@ public class CustomerController {
     private CustomerRepository customerRepository;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String customers(Model model){
         List<Customer> customerList = customerRepository.findAll();
         model.addAttribute("customers", customerList);
