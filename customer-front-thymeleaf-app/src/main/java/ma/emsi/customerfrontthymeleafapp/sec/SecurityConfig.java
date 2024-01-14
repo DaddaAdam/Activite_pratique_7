3f15a926c0f47a46f6fdcff8b7d4f629c67642cd
DaddaAdam <daddaadam@gmail.com>
2024-01-14 18:00:39 +0100

    Added oauth2 with keycloak

--- customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/sec/SecurityConfig.java
+++ customer-front-thymeleaf-app/src/main/java/ma/emsi/customerfrontthymeleafapp/sec/SecurityConfig.java
@@ -5,11 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
@@ -18,9 +26,17 @@ public class SecurityConfig {
                 .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
                 .logout((logout) -> logout
-                        //.logoutSuccessHandler(oidcLogoutSuccessHandler())
+                        .logoutSuccessHandler(oidcLogoutSuccessHandler())
                         .logoutSuccessUrl("/").permitAll()
+                        .clearAuthentication(true)
                         .deleteCookies("JSESSIONID"))
                 .build();
+    }
+
+    private OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        final OidcClientInitiatedLogoutSuccessHandler oidcClientInitiatedLogoutSuccessHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(this.clientRegistrationRepository);
+        oidcClientInitiatedLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:8083/");
+        return oidcClientInitiatedLogoutSuccessHandler;
     }
 }
