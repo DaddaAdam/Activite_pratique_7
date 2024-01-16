8d20f03d094448e893ff4b4a38b39ba457d3b605
DaddaAdam <daddaadam@gmail.com>
2024-01-16 16:53:18 +0100

    Fixed cors origins issue

--- customer-front-angular-app/src/app/products/products.component.ts
+++ customer-front-angular-app/src/app/products/products.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products: any;
+  constructor(private http: HttpClient) {
+  }
 
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:8098/products').subscribe(data => {
+      this.products = data;
+    })
+  }
 }
